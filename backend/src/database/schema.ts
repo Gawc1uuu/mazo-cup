db42a5946c24d45fd3feffa8522e50a273a5f9e7
Gawc1uuu <gawlikjakub9@gmail.com>
2024-12-22 11:51:33 +0100

    games migrations

--- backend/src/database/schema.ts
+++ backend/src/database/schema.ts
@@ -1,4 +1,7 @@
-import { pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+import { boolean, pgEnum, pgTable, timestamp, uuid, varchar } from "drizzle-orm/pg-core"
+
+const statusEnum = pgEnum("status", ["waiting", "picking_teams", "ready"]);
+
 
 export const UserTable = pgTable("users", {
     id: uuid("id").primaryKey().defaultRandom(),
@@ -6,4 +9,20 @@ export const UserTable = pgTable("users", {
     email: varchar("email", { length: 255 }).unique().notNull(),
     password: varchar("password", { length: 255 }).notNull(),
     createdAt: timestamp("createdAt").defaultNow()
-}) 
+})
+
+export const GamesTable = pgTable("games", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    status: varchar("status", { length: 50 }),
+    capitain1Id: uuid("capitain1_id").references(() => UserTable.id),
+    capitain2Id: uuid("capitain2_id").references(() => UserTable.id),
+    createdAt: timestamp("createdAt").defaultNow()
+})
+
+export const GamePlayersTable = pgTable("game_players", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    gameId: uuid("game_id").references(() => GamesTable.id),
+    playerId: uuid("player_id").references(() => UserTable.id),
+    team: varchar("team", { length: 50 }).default("none"),
+    isCapitain: boolean("is_captain").default(false)
+})
