c90250eb96b1024dd9f1987246f2632e43f48e07
Gawc1uuu <gawlikjakub9@gmail.com>
2024-12-22 13:50:23 +0100

    simple socket connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,12 @@
 import express from 'express';
-const app = express();
 import cors from "cors";
 import authRouter from "./routes/authRoutes"
+import { createServer } from "http"
+import { Server } from "socket.io"
+
+
+const app = express();
+
 
 app.use(cors())
 app.use(express.json())
@@ -9,6 +14,24 @@ app.use(express.urlencoded())
 app.use(express.text())
 app.use("/api/user", authRouter)
 
-app.listen(4000, () => {
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000', // Allow requests from React app
+    methods: ['GET', 'POST'],       // Allowed HTTP methods
+    credentials: true,
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('A client connected');
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  });
+});
+
+
+server.listen(4000, () => {
   console.log('Server is running on port 4000');
 });
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { io } from "socket.io-client";
+
+
 
 const Dashboard = () => {
+
+    useEffect(() => {
+
+        const socket = io("http://localhost:4000", {
+            withCredentials: true,
+            transports: ['websocket', 'polling'],
+        });
+
+        socket.on('connect', () => {
+            console.log('Connected to server');
+        });
+
+        socket.on('disconnect', () => {
+            console.log('Disconnected from server');
+        });
+
+        // Cleanup on component unmount
+        return () => {
+            socket.disconnect();
+        };
+    }, [])
+
     return (
         <div>Dashboard</div>
     )
